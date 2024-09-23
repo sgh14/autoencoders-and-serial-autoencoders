30013dad16e69b05e65930e19397f09278b85902
Sergio García Heredia <sergio.gh14@gmail.com>
2024-09-23 13:33:19 +0200

    Reorganize output folders and implement all the metrics

--- experiments/swiss_roll/experiment.py
+++ experiments/swiss_roll/experiment.py
@@ -1,3 +1,5 @@
+import time
+import tensorflow as tf
 from os import path
 
 from Autoencoder import Autoencoder
@@ -6,7 +8,8 @@ from experiments.swiss_roll.load_data import get_datasets
 from experiments.utils import build_encoder, build_decoder
 from experiments.swiss_roll.metrics import compute_metrics
 
-
+seed = 123
+tf.random.set_seed(seed)
 root = 'experiments/swiss_roll/results'
 titles = [
     'Few samples without noise',
@@ -14,29 +17,44 @@ titles = [
     'Few samples with noise',
     'Many samples with noise'
 ]
-datasets_train, datasets_test = get_datasets(npoints=2000, test_size=0.5, seed=123, noise=0.5)
-for (X, y), title in zip(datasets_train, titles):
-    plot_original(X, y, title, path.join(root, 'train_orig'))
-
-for (X, y), title in zip(datasets_test, titles):
-    plot_original(X, y, title, path.join(root, 'test_orig'))
+datasets_train, datasets_test = get_datasets(npoints=2000, test_size=0.5, seed=seed, noise=0.5)
 
 for (X_train, y_train), (X_test, y_test), title in zip(datasets_train, datasets_test, titles):
     encoder = build_encoder(input_shape=(X_train.shape[-1],), units=128, n_components=2)
     decoder = build_decoder(output_shape=(X_train.shape[-1],), units=128, n_components=2)
     autoencoder = Autoencoder(encoder, decoder)
+    tic = time.perf_counter()
     autoencoder.compile(optimizer='adam', loss='mse')
-    history = autoencoder.fit(X_train, epochs=300, validation_split=0.1, shuffle=True, batch_size=64, verbose=0)
+    history = autoencoder.fit(X_train, epochs=500, validation_split=0.1, shuffle=True, batch_size=64, verbose=0)
 
     X_train_red = autoencoder.encode(X_train)
+    tac = time.perf_counter()
     X_test_red = autoencoder.encode(X_test)
+    toc = time.perf_counter()
     X_train_rec = autoencoder.decode(X_train_red)
     X_test_rec = autoencoder.decode(X_test_red)
 
-    plot_projection(X_train_red, y_train, title, path.join(root, 'train_red'))
-    plot_original(X_train_rec, y_train, title, path.join(root, 'train_rec'))
-    plot_projection(X_test_red, y_test, title, path.join(root, 'test_red'))
-    plot_original(X_test_rec, y_test, title, path.join(root, 'test_rec'))
+    plot_original(X_train, y_train, path.join(root, title), 'train_orig')
+    plot_original(X_test, y_test, path.join(root, title), 'test_orig')
+    plot_projection(X_train_red, y_train, path.join(root, title), 'train_red')
+    plot_original(X_train_rec, y_train, path.join(root, title), 'train_rec')
+    plot_projection(X_test_red, y_test, path.join(root, title), 'test_red')
+    plot_original(X_test_rec, y_test, path.join(root, title), 'test_rec')
     plot_history(history, path.join(root, 'histories', title), log_scale=True)
 
-    compute_metrics(X_test, X_test_rec, title, root)        
+    time_in_sample = tac - tic
+    time_out_of_sample = toc - tac
+    compute_metrics(
+        X_train,
+        X_train_red,
+        X_train_rec,
+        X_test,
+        X_test_red,
+        X_test_rec,
+        time_in_sample,
+        time_out_of_sample,
+        title,
+        output_dir=path.join(root, title)
+    )
+
+   
